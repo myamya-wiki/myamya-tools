1d2b048fb5c06410c9815fe381824d76e0c3f8b6
みゃみゃ <115388782+myamya-wiki@users.noreply.github.com>
2023-07-20 11:33:36 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
-var player;
-
-function onYouTubeIframeAPIReady() {
-    player = new YT.Player('player', {
-        height: '360',
-        width: '640',
-        videoId: '',
-        events: {
-            'onReady': onPlayerReady,
-        }
-    });
-}
-
-function onPlayerReady(event) {
-    // プレイヤーが準備完了の際に呼ばれるコールバック
-    console.log('Player is ready!');
-}
+google.load("client", "1", {
+    callback: function () {
+        // Google APIクライアントを初期化
+        gapi.client.init({
+            apiKey: 'AIzaSyCFMb23YnOjTKv1ucVWJrSc6I0tQBYIlTI',
+            discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest"]
+        });
+    }
+});
 
 function loadVideo() {
     var videoId = document.getElementById('videoIdInput').value;
-    player.loadVideoById(videoId);
+
+    // YouTube Data APIを使用して動画の情報を取得
+    gapi.client.youtube.videos.list({
+        part: 'player',
+        id: videoId
+    }).then(function (response) {
+        var videoPlayerUrl = response.result.items[0].player.embedHtml;
+        var playerDiv = document.getElementById('player');
+
+        // 動画を再生するiframeを生成して表示
+        playerDiv.innerHTML = videoPlayerUrl;
+    }, function (error) {
+        console.error('Error:', error);
+    });
 }
